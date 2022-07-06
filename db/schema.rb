11b37ff2a295b3a54a8339880481ea0c41ac1ff3
terakura.aina <aicaierwei@gmail.com>
2022-07-06 21:56:48 +0900

    テーブル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_07_06_125520) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+  end
+
+  create_table "ideas", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.text "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
---
+++ spec/factories/ideas.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :idea do
+    categoryId { "" }
+    body { "MyText" }
+  end
+end
